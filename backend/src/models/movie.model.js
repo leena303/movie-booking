cc7a5f905e814b4dfc4db0f8e819882c44b446ca
leena303 <lethisuna303@gmail.com>
2026-04-12 23:34:27 +0700

    fix(model): update movie.model

--- backend/src/models/movie.model.js
+++ backend/src/models/movie.model.js
@@ -23,26 +23,7 @@ const MovieModel = {
     sql += " ORDER BY id DESC";
 
     const [movies] = await pool.query(sql, params);
-
-    const [showtimes] = await pool.query(`
-      SELECT 
-        s.id,
-        s.movie_id,
-        s.room_id,
-        s.start_time,
-        s.price,
-        r.name AS room_name
-      FROM showtimes s
-      JOIN rooms r ON s.room_id = r.id
-      ORDER BY s.start_time ASC
-    `);
-
-    const moviesWithShowtimes = movies.map((movie) => ({
-      ...movie,
-      showtimes: showtimes.filter((s) => s.movie_id === movie.id),
-    }));
-
-    return moviesWithShowtimes;
+    return movies;
   },
 
   async getById(id) {
