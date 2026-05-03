a32f6d7dd6be74fe4c54cf76c52a72302ea03a2f
leena303 <lethisuna303@gmail.com>
2026-05-03 22:22:01 +0700

    feat(auth): define authentication routes

--- backend/src/routes/auth.routes.js
+++ backend/src/routes/auth.routes.js
@@ -1,10 +1,14 @@
 const express = require("express");
 const router = express.Router();
+
 const authController = require("../controllers/auth.controller");
+const authMiddleware = require("../middleware/auth.middleware");
 
 router.post("/register", authController.register);
 router.post("/login", authController.login);
 router.post("/forgot-password", authController.forgotPassword);
 router.post("/reset-password", authController.resetPassword);
+
+router.put("/me", authMiddleware, authController.updateMe);
 
 module.exports = router;
